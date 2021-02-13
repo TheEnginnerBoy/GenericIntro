9425ca5959216fb4d791c6e810ddbe5688942737
TheEnginnerBoy <78681893+TheEnginnerBoy@users.noreply.github.com>
2021-02-13 14:29:50 +0300

    Add files via upload

---
+++ GenericIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+
+            isimler.Add("Ali");
+            isimler.Add("Veli");
+            isimler.Add("Ahmet");
+            isimler.Add("Mehmet");
+            isimler.Add("Kadir");
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+        }
+    }
+}
